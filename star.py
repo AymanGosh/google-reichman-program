4b73990bc4f3ce380633ea0c694480bbf8d3b986
aymangosh <aymansce@gmail.com>
2025-01-23 14:18:51 +0200

    change the size and write message at the end

--- star.py
+++ star.py
@@ -6,9 +6,10 @@ color("green", "blue")
 
 begin_fill()
 while True:
-    forward(200)
+    forward(50)
     left(160)
     if abs(pos()) < 1:
         break
 end_fill()
+write("may the force be with you!", align="right")
 exitonclick()
