d3529abbea0bb0ab286595389024da23d6a400fc
aymangosh <aymansce@gmail.com>
2025-01-23 14:15:47 +0200

    change speed and location

--- star.py
+++ star.py
@@ -1,12 +1,12 @@
 from turtle import *
-
+speed(10)
 
 color("green", "blue")
 
 
 begin_fill()
 while True:
-    forward(200)
+    forward(300)
     left(160)
     if abs(pos()) < 1:
         break
